5712570a52d2bb931343b34940b7e3d71ef6c43b
Matthew Seidel <matthew.seidel@siesa.com>
2021-05-20 09:17:10 -0500

    migrations

--- console/migrations/m130524_201442_init.php
+++ console/migrations/m130524_201442_init.php
@@ -4,7 +4,7 @@ use yii\db\Migration;
 
 class m130524_201442_init extends Migration
 {
-    public function up()
+    public function safeUp()
     {
         $tableOptions = null;
         if ($this->db->driverName === 'mysql') {
@@ -12,22 +12,49 @@ class m130524_201442_init extends Migration
             $tableOptions = 'CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE=InnoDB';
         }
 
-        $this->createTable('{{%user}}', [
+        $this->createTable('user', [
             'id' => $this->primaryKey(),
             'username' => $this->string()->notNull()->unique(),
             'auth_key' => $this->string(32)->notNull(),
             'password_hash' => $this->string()->notNull(),
             'password_reset_token' => $this->string()->unique(),
             'email' => $this->string()->notNull()->unique(),
-
+            'user_type' => $this->integer()->defaultValue(1),
+            'phone' => $this->string(10)->unique(),
             'status' => $this->smallInteger()->notNull()->defaultValue(10),
             'created_at' => $this->integer()->notNull(),
             'updated_at' => $this->integer()->notNull(),
         ], $tableOptions);
+
+
+        $this->createTable('product', [
+            'id' => $this->primaryKey(),
+            'title' => $this->string()->notNull(),
+            'description' => $this->text(),
+            'price' => $this->integer()->notNull(),
+        ], $tableOptions);
+
+        $this->createTable('discount', [
+            'id' => $this->primaryKey(),
+            'productId' => $this->integer(),
+            'start_date' => $this->date(),
+            'end_date' => $this->date(),
+        ], $tableOptions);
+
+        $this->addForeignKey(
+            'fk_discount_product',
+            'discount',
+            'productId',
+            'product',
+            'id'
+        );
     }
 
     public function down()
     {
-        $this->dropTable('{{%user}}');
+        $this->removeForeignKey('fk_discount_product');
+        $this->dropTable('discount');
+        $this->dropTable('product');
+        $this->dropTable('user');
     }
 }
